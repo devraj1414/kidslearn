4d92536a8b65077c4280f74aa649991643555401
Rajkumar Singh <devarj1414@gmail.com>
2022-04-03 11:19:12 -0400

    Initial Commit

---
+++ Shared/KidsLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  KidsLearningApp.swift
+//  Shared
+//
+//  Created by Suruchi Kumari on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct KidsLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
